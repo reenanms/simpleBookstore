919c397173e48e44d8de5e24fea68ef0d0601c58
Márcia Santos <csantos_marcia@hotmail.com>
2021-06-19 00:15:39 -0300

    simple header and templates seteds

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/livros')
+def main_page():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('main_page.html', user=user, posts=posts)
+
+@app.route('/about')
+def about_page():
+    return render_template('sobre_nos.html')
+
+@app.route('/carrinho')
+def car_page():
+    return render_template('carrinho.html')
+
+@app.route('/vendedor')
+def login_vendedor():
+    return render_template('acesso_vendedor.html')
 
 
 if __name__ == '__main__':
