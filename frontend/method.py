dcb43d1fd6375fe38aa4492c5418af25280c9b9b
Renan Martins <renan.silva@klassmatt.com>
2021-06-22 22:15:40 -0300

    Integração da rota de livro do front com o backend

--- frontend/method.py
+++ frontend/method.py
@@ -1,32 +1,8 @@
+import requests
+
+URL='http://localhost:3000'
+
 def get_livros():
-    livros = [
-        {
-            'id': '128391',
-            'titulo': 'O sol é para todos',
-            'autor': 'Jose da silva',
-            'preco': '18.90',
-            'estoque': '1'
-        },
-        {
-            'id': '11548',
-            'titulo': 'Eu amo Joana',
-            'autor': 'João da silva',
-            'preco': '15.90',
-            'estoque': '1'
-        },
-        {
-            'id': '85468',
-            'titulo': 'A vida',
-            'autor': 'Maria da silva',
-            'preco': '120.00',
-            'estoque': '1'
-        },
-        {
-            'id': '15454',
-            'titulo': 'A vida',
-            'autor': 'Lupin',
-            'preco': '120.00',
-            'estoque': '0'
-        }
-    ]
+    r = requests.get(url = URL+'/livro')
+    livros = r.json()
     return livros
---
+++ frontend/run.bat
@@ -0,0 +1,4 @@
+set FLASK_APP=app.py
+set FLASK_ENV=development
+python -m flask run -h localhost -p 5000
+pause
