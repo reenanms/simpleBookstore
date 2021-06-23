19f20051195e4f51433100741e6523c0c44c2440
Márcia Santos <csantos_marcia@hotmail.com>
2021-06-23 01:31:42 -0300

    adicionado estrutura pra sessao e pediso

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask_cors import CORS
+from flask_session import Session
 import method as mt
 
 app = Flask(__name__)
+app.secret_key = 'BAD_SECRET_KEY'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+CORS(app)
 
 @app.route('/')
 @app.route('/livros')
@@ -9,25 +16,46 @@ def main_page():
     livros = mt.get_livros()
     return render_template('main_page.html', livros=livros)
 
+
 @app.route('/sobre')
 def about_page():
     return render_template('sobre_nos.html')
+
 
 @app.route('/carrinho')
 def car_page():
     return render_template('carrinho.html')
 
+
 @app.route('/vendedor')
 def acesso_vendedor():
     return render_template('acesso_vendedor.html')
+
 
 @app.route('/pedidos')
 def pedidos():
     return render_template('pedidos.html')
 
+
 @app.route('/cadastro')
 def cadastro_livro():
     return render_template('cadastro_livro.html')
+
+
+@app.route('/add-car/<livroId>')
+def add_car(livroId):
+    if not session.get('pedidoId'):
+        session['pedidoId'] = mt.get_pedido_id()
+    if session.get('pedidoId'):
+        if mt.add_pedido(livroId) == 'ok':
+            print('Adicionado ao pedido com sucesso')
+        else:
+            print('PROBLEMA ao adicionar ao pedido')
+
+    return main_page()
+
+
+
 
 if __name__ == '__main__':
     app.run()
--- frontend/method.py
+++ frontend/method.py
@@ -3,6 +3,37 @@ import requests
 URL='http://localhost:3000'
 
 def get_livros():
+    '''
     r = requests.get(url = URL+'/livro')
     livros = r.json()
+    print(livros)
+    '''
+    livros = [
+        {
+            'id': '128391',
+            'titulo': 'O sol é para todos',
+            'autor': 'Jose da silva',
+            'preco': '18.90',
+        },
+        {
+            'id': '11548',
+            'titulo': 'Eu amo Joana',
+            'autor': 'João da silva',
+            'preco': '15.90',
+        },
+        {
+            'id': '85468',
+            'titulo': 'A vida',
+            'autor': 'Maria da silva',
+            'preco': '120.00',
+        }
+    ]
     return livros
+
+def add_pedido(id):
+    #fazer post do livro
+    return 'ok'
+
+def get_pedido_id():
+    #fazer get do id do pedido
+    return ('1')
